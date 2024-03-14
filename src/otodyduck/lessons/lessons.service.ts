0e29fcd008861270a8e1f0a405e1f7ac0074bac0
Aldino Efendi <aldino.efendi@ist.id>
2024-03-14 22:16:40 +0700

    update api CRUD for lesson

--- src/otodyduck/lessons/lessons.service.ts
+++ src/otodyduck/lessons/lessons.service.ts
@@ -29,26 +29,31 @@ export class LessonsService {
     const chapter = await this.chapterRepository.findOne({ where: { id: request.chapter_id } })
     if (!chapter) throw new HttpException(ResponseMessage.CHAPTER_NOT_FOUND, HttpStatus.NOT_FOUND)
 
-    const lesson = this.lessonRepository.create(request)
-    lesson.chapter = chapter
+    const lesson = this.lessonRepository.create({
+      ...request,
+      videoUrl: request.video,
+      chapter: chapter
+    })
 
     const newLesson = await this.lessonRepository.save(lesson)
     return new Result(ResponseMessage.SUCCESS, newLesson)
   }
 
   async updateLesson(id: number, request: RequestOtodyduckLessonDTO) {
-    const { chapter_id, ...lessonRequest } = request
+    const { chapter_id, video, ...lessonRequest } = request
     const lesson = await this.lessonRepository.findOne({ where: { id } })
     if (!lesson) throw new HttpException(ResponseMessage.LESSON_NOT_FOUND, HttpStatus.NOT_FOUND)
 
     const chapter = await this.chapterRepository.findOne({ where: { id: chapter_id } })
     if (!chapter) throw new HttpException(ResponseMessage.CHAPTER_NOT_FOUND, HttpStatus.NOT_FOUND)
+    
+    await this.lessonRepository.update(lesson.id, {
+      ...lessonRequest,
+      chapter: chapter,
+      videoUrl: video
+    })
 
-    lesson.updatedAt = new Date()
-    lesson.chapter = chapter
-    Object.assign(lesson, lessonRequest)
-    const updatedLesson = await this.lessonRepository.save(lesson)
-    return new Result(ResponseMessage.SUCCESS, updatedLesson)
+    return new Result(ResponseMessage.SUCCESS, await this.lessonRepository.findOneBy({ id }))
   }
 
   async deleteLesson(id: number) {
