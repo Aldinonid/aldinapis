75b28a068cc926bc6234cb78b7109ac40e8310a3
Aldin <aldino.efendi@icloud.com>
2025-09-26 13:32:24 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { StaycationModule } from './staycation/staycation.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseConfig } from './config/typeorm.config';
 import { config } from './config/configuration';
 import { OtodyduckModule } from './otodyduck/otodyduck.module';
@@ -14,11 +14,11 @@ import { JwtModule } from '@nestjs/jwt';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       // useClass: DatabaseConfig
-      useFactory: (config: ConfigService) => ({
+      useFactory: () => ({
         type: 'postgres',
-        url: config.get<string>('postgresql://Aldinonid:Yk0RF5paENrB@ep-steep-cloud-a1ogi14z-pooler.ap-southeast-1.aws.neon.tech/portfolio?sslmode=require&channel_binding=require'),
+        url: 'postgresql://Aldinonid:Yk0RF5paENrB@ep-steep-cloud-a1ogi14z-pooler.ap-southeast-1.aws.neon.tech/portfolio?sslmode=require&channel_binding=require',
         autoLoadEntities: true, // otomatis load entity
-        synchronize: true,      // ❗ hanya untuk dev, jangan dipakai di production
+        synchronize: false,      // ❗ hanya untuk dev, jangan dipakai di production
         ssl: {
           rejectUnauthorized: false, // wajib di Neon karena butuh SSL
         },
